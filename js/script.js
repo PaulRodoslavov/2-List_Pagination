7e88e51ffb3c90b9ba5ee04eca43e77d12166619
Pavlo Rodoslavov <pasharodoslavov@MacBook-Air-Pavlo.local>
2018-10-19 22:21:22 -0700

    add features

--- js/script.js
+++ js/script.js
@@ -8,11 +8,12 @@ FSJS project 2 - List Filter and Pagination
 const people = document.getElementsByClassName('student-item');
 const search = document.querySelector('.page-header');
 const page = document.querySelector('.page');
-const navigation = document.createElement('UL');
-let pagination = document.createElement("DIV");
+
 let arrStudents = [...people];
+// console.log(arrStudents);
 const alert = document.createElement("DIV");
 const studentDet = document.querySelectorAll('.student-item');
+
 
 //variable for alert no match
 alert.innerHTML = 'There is no match';
@@ -43,42 +44,58 @@ function showTenStudents(arr, num1) {
 
    alert.classList.add('hide');
    hideListAll (arr);
-
-   // arr.forEach((item)=>{
-   //    item.classList.add("hide");
-   // });
-
    for(let i = num1 - 10; i < num1; i++){
-      console.log(num1);
+      // console.log(arr);
       if(arr[i]){
+         // console.log(arr[i]);
          arr[i].classList.remove('hide');
       }
    }
 }
 
 // Create and append the pagination links
+function createLinks (arr) {
+   // console.log(arr);
+   const some = document.querySelector('.pagination');
+   if (some){
+      some.remove();
+   }
+   const navigation = document.createElement('UL');
+   let pagination = document.createElement("DIV");
+   pagination.classList.add('pagination');
 
-pagination.classList.add('pagination');
-for(let i = 1; i <= Math.ceil(people.length / 10); i++){
-   let link = `<li><a href="#">${i}</a></li>`;
-   navigation.innerHTML += link;
+
+   for(let i = 1; i <= Math.ceil(arr.length / 10); i++){
+      let link = `<li><a href="#">${i}</a></li>`;
+      navigation.innerHTML += link;
+   }
+
+   pagination.appendChild(navigation);
+   page.appendChild(pagination);
+
+   const navBar = document.querySelector('.pagination');
+   if(navBar.firstChild.firstChild) {
+      navBar.firstChild.firstChild.firstChild.classList.add('active');
+   }
 }
-pagination.appendChild(navigation);
-page.appendChild(pagination);
+createLinks (people);
 
-// logic to show particular items by clicking link
+// logic to show particular 10 items by clicking link
 
-const navBar = document.querySelector('.pagination');
-navBar.firstChild.firstChild.firstChild.classList.add('active');
+document.querySelector('.pagination').addEventListener('click', (event) => {
+   logicToShowStudents(arrStudents);
 
-navBar.addEventListener('click', (event) => {
+});
+
+function logicToShowStudents(arr) {
    if(event.target.tagName === 'A'){
+      console.log(event.target);
       const active = document.querySelector('.active');
       active.classList.remove("active");
       event.target.classList.add('active');
-      showTenStudents(arrStudents, event.target.innerHTML + '0');
+      showTenStudents(arr, event.target.innerHTML + '0');
    }
-});
+}
 
 // create serach field
 
@@ -94,6 +111,7 @@ const searchId = document.querySelector('#search');
 
 
 searchId.addEventListener('click',()=>{
+   let arrMatch = [];
    alert.classList.add('hide');
    const input = document.querySelector('#input').value;
    if (input.length > 0) {
@@ -104,6 +122,8 @@ searchId.addEventListener('click',()=>{
          let found = h3.match(input);
          if(found) {
             el.classList.remove('hide');
+            arrMatch.push(el);
+            el.classList.add('view');
             match++;
          }
       });
@@ -111,20 +131,20 @@ searchId.addEventListener('click',()=>{
          alert.classList.remove('hide');
          search.appendChild(alert);
       }
+      const matchStudent = [...document.querySelectorAll('.view')];
+
+      // delete class view
+      createLinks (matchStudent);
+      matchStudent.forEach((el) => {el.classList.remove('view')});
+
+      // logic to show particular matched 10 items by clicking link
+
+      hideList(arrMatch);
+      document.querySelector('.pagination').addEventListener('click', (event) => {
+         logicToShowStudents(arrMatch);
+      });
    }
 })
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
